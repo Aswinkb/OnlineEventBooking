5ca3bdf4fba3f52a94cb42dc799a23bd46d8ff5d
unknown <aswinhss123@gmail.com>
2023-07-13 22:16:29 +0530

    f1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'landingPage', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) },
+                        {path:'',redirectTo:'landingPage',pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
